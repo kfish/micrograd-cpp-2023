d41a96e3bb15905e5d19cf8413767a73677334b0
Conrad Parker <conrad@metadecks.org>
2023-09-22 06:42:00 +1000

    tests: expand Value

--- tests/value-test.cpp
+++ tests/value-test.cpp
@@ -6,7 +6,17 @@ using namespace ai;
 
 int main(int argc, char *argv[])
 {
-    Value<double> x(2.0);
+    Value<double> a(2.0);
+    Value<double> b(-3.0);
+    Value<double> c(10.0);
 
-    std::cout << x << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << (a+b) << std::endl;
+    std::cout << (a*b) << std::endl;
+
+    std::cout << (a-b) << std::endl;
+    std::cout << (a/b) << std::endl;
+
+    std::cout << (a*b + c) << std::endl;
 }
