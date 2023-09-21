6cc185a1ae4c93e39915d4832d7f67c4cdda6d43
Conrad Parker <conrad@metadecks.org>
2023-09-22 06:41:41 +1000

    Value: add arith ops

--- include/value.h
+++ include/value.h
@@ -14,13 +14,29 @@ class Value {
         const T& data() const {
             return data_;
         }
+
+        Value<T> operator+(const Value<T>& other) const {
+            return Value(data_ + other.data());
+        }
+
+        Value<T> operator-(const Value<T>& other) const {
+            return Value(data_ - other.data());
+        }
+
+        Value<T> operator*(const Value<T>& other) const {
+            return Value(data_ * other.data());
+        }
+
+        Value<T> operator/(const Value<T>& other) const {
+            return Value(data_ / other.data());
+        }
     private:
         T data_;
 };
 
 template <typename T>
 static inline std::ostream& operator<<(std::ostream& os, const Value<T>& value) {
-    return os << value.data();
+    return os << "Value(data=" << value.data() << ")";
 }
 
 }
