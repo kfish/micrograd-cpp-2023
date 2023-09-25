bb76b73d33ae0e33b6afc71fc86eccf5bebae99f
Conrad Parker <conrad@metadecks.org>
2023-09-26 09:32:16 +1000

    tests: add mac-test

---
+++ tests/mac-test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "quickmath.h"
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> vec_a = {1, 2, 3};
+    std::vector<int> vec_b = {4, 5, 6};
+    std::array<int, 3> arr_a = {1, 2, 3};
+    std::array<int, 3> arr_b = {4, 5, 6};
+
+    std::cout << "Vector multiply-accumulate: " << mac(vec_a, vec_b) << std::endl;
+    std::cout << "Array multiply-accumulate: " << mac(arr_a, arr_b) << std::endl;
+}
+
