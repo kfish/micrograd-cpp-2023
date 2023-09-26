3d927c2c7de52ee1592f6aa18031e72521166514
Conrad Parker <conrad@metadecks.org>
2023-09-26 11:06:03 +1000

    funcy: update map_array

--- include/funcy.h
+++ include/funcy.h
@@ -5,11 +5,10 @@
 #include <iostream>
 #include <functional>  // for std::invoke
 
-template<typename T, typename Arg, std::size_t N>
-std::array<T, N> map_array(const std::array<std::function<T(Arg)>, N>& input, const Arg& arg) {
-    std::array<T, N> output;
-    std::transform(std::execution::par, input.begin(), input.end(), output.begin(),
-                   [&arg](const auto& func) { return std::invoke(func, arg); });
+template <typename F, typename Tin, typename Tout, size_t N>
+auto map_array(const std::array<F, N>& arr, const Tin& input) -> std::array<Tout, N> {
+    std::array<Tout, N> output{};
+    std::transform(std::execution::par_unseq, arr.begin(), arr.end(), output.begin(),
+                   [&](const F& f) { return f(input); });
     return output;
 }
-
