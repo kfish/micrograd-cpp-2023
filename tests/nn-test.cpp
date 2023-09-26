c8bc8fac05c4570fbd91cf56e463a08f9c922819
Conrad Parker <conrad@metadecks.org>
2023-09-26 11:06:49 +1000

    nn-test: update neuron, layer

--- tests/nn-test.cpp
+++ tests/nn-test.cpp
@@ -6,12 +6,16 @@ using namespace ai;
 
 int main(int argc, char *argv[])
 {
-    Neuron<double, 2> n;
+    std::array<double, 3> input = {0.1, 0.2, 0.3};
+
+    Neuron<double, 3> n;
 
     std::cerr << n << std::endl;
+    std::cerr << n(input) << std::endl;
 
-    Layer<double, 2, 3> l;
+    Layer<double, 3, 4> l;
 
     std::cerr << l << std::endl;
+    std::cerr << PrettyArray(l(input)) << std::endl;
 }
 
