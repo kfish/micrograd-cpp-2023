8e3d393360384c027998739362ca21bf2729d121
Conrad Parker <conrad@metadecks.org>
2023-10-11 09:56:08 +1100

    binary-classifier: learn 1.0

--- examples/binary-classifier.cpp
+++ examples/binary-classifier.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
     std::cerr << "y (gt):\t" << PrettyArray(y) << std::endl;
 
     // Run backprop
-    double learning_rate = 0.01;
+    double learning_rate = 1.0;
 
     auto backprop = BackProp<double, 4, std::array<double, 3>>(n, "loss.tsv");
     double loss = backprop(input, y, learning_rate, 1000, true);
