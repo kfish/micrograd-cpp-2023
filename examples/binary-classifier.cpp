7f94dae5dc3ca02e99ed90943f57b33f92486226
Conrad Parker <conrad@metadecks.org>
2023-10-14 19:22:58 +1100

    binary-classifier: increase learning rate

--- examples/binary-classifier.cpp
+++ examples/binary-classifier.cpp
@@ -24,9 +24,9 @@ int main(int argc, char *argv[])
     std::cerr << "y (gt):\t" << PrettyArray(y) << std::endl;
 
     // Run backprop
-    double learning_rate = 0.3;
+    double learning_rate = 0.9;
 
     auto backprop = BackProp<double, 4, std::array<double, 3>>(n, "loss.tsv");
-    double loss = backprop(input, y, learning_rate, 1000, true);
+    double loss = backprop(input, y, learning_rate, 100, true);
 }
 
