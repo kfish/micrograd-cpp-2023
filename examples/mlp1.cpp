a822f7f492ea115ad1d9524f6b64ab8d9c0df2f5
Conrad Parker <conrad@metadecks.org>
2023-10-19 06:32:10 +1100

    mlp1: only generate mlp1 graph

--- examples/mlp1.cpp
+++ examples/mlp1.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 
-#include "randomdata.h"
 #include "nn.h"
+#include "graph.h"
 
 using namespace ai;
 
 int main(int argc, char *argv[])
 {
-    std::array<Value<double>, 3> input = {make_value(0.1), make_value(0.2), make_value(0.3)};
+    // Define a neural net
+    MLP1<double, 3, 4, 4, 1> n;
 
-    Neuron<double, 3> n;
+    std::array<double, 3> input = {{ 2.0, 3.0, -1.0 }};
+    auto output = n(input);
 
-    std::cerr << n << std::endl;
-    std::cerr << n(input) << std::endl;
+    backward(output);
 
-    Layer<double, 3, 4> l;
-
-    std::cerr << l << std::endl;
-    std::cerr << PrettyArray(l(input)) << std::endl;
-
-    MLP<double, 3, 4, 2> mlp;
-    std::cerr << mlp << std::endl;
-    std::cerr << PrettyArray(mlp(input)) << std::endl;
+    std::cout << Graph(output) << std::endl;
 }
+
 
